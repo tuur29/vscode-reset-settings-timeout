ee282e554ae45345d5d065607a3ce8c1280cef0e
Tuur Lievens <tuur.lievens@hotmail.com>
2019-10-09 18:28:11 +0200

    Sketch out planned functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,37 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "reset-settings-timeout" is now active!');
+	// TODO: check if settings are configured -> start timeout
+	// TODO: check if timeout has passed during shutdown -> reset settings
+	// TODO: show notification on reset if preferred
+	// TODO: reset settings in appropriate file: local resetSettings config should clear local, user should clear user
+
+	/*
+	
+		Settings format:
+
+		{
+			settings: {
+				...
+			},
+			timeout: 3600,
+			resetOnRestart: true,
+			notifyOfReset: false.
+		}
+
+	 */
 
 	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
+		// TODO: create command to reset settings manually
+		// TODO: create command to reset timer
 		vscode.window.showInformationMessage('Hello World!');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	// TODO: cleanup timers
+}
