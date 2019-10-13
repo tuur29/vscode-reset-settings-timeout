4b1027ffecf362b4ff50973d1aa54a75b1e4e76e
Tuur Lievens <tuur.lievens@hotmail.com>
2019-10-13 15:47:33 +0200

    Implement basic version with commands

--- src/extension.ts
+++ src/extension.ts
@@ -5,30 +5,41 @@ let timer: NodeJS.Timeout | null = null;
 //#region LIFECYCLE
 
 export function activate(context: vscode.ExtensionContext) {
+	debug("activate extension");
+	const settings = getSettings();
+	if (settings.resetOnRestart) {
+		resetSettings(true);
+	}
 
 	setupTimer();
 
-	const settings = getSettings();
-	if (settings.resetOnRestart) {
-		resetSettings();
-	}
+	vscode.workspace.onDidChangeConfiguration(() => {
+		clearTimer();
+		setupTimer();
+	});
 
 	// TODO: check if timeout has passed during shutdown -> reset settings
 	
 	// define commands
-	let resetNow = vscode.commands.registerCommand('reset-settings-timeout.resetNow', () => {
+	const resetNow = vscode.commands.registerCommand('reset-settings-timeout.resetNow', () => {
+		debug("command: resetNow");
+		clearTimer();
 		resetSettings();
+		setupTimer();
+
 	});
 	context.subscriptions.push(resetNow);
 
-	let restartTimer = vscode.commands.registerCommand('reset-settings-timeout.restartTimer', () => {
-		resetTimer();
-		
+	const restartTimer = vscode.commands.registerCommand('reset-settings-timeout.restartTimer', () => {
+		debug("Ccommand: restartTimer");
+		clearTimer();
+		setupTimer();
 	});
 	context.subscriptions.push(restartTimer);
 }
 
 export function deactivate() {
+	debug("deactivate extension");
 	clearTimer();
 }
 
@@ -36,37 +47,46 @@ export function deactivate() {
 
 //#region ACTIONS
 
-function setupTimer() {
+function resetSettings(silent = false) {
 	const settings = getSettings();
 
-	if (Object.entries(settings.settings).length > 0) {
-		timer = setTimeout(() => {
-			
-		}, settings.timeout);
+	debug("reset: execute commands");
+	settings.commands.forEach(command => {
+		vscode.commands.executeCommand(command);
+	});
+
+	// debug("reset: settings");
+	// TODO: reset settings in appropriate file: local resetSettings config should clear local, user should clear user
+	// Object.entries((setting: string, value: any) => {
+	// });
+
+	if (settings.notifyOfReset && !silent) {
+		debug("reset: show notification");
+		vscode.window.showInformationMessage('Settings have been reset!');
 	}
 }
 
-function resetTimer() {
-	// TODO: create command to restart timer
+function setupTimer() {
+	const settings = getSettings();
+
+	if (
+		(Object.entries(settings.settings).length > 0 || Object.entries(settings.commands).length) > 0
+		&& settings.timeout > 10 // TODO: change min timeout to 60
+	) {
+		debug("timer: start");
+		timer = setTimeout(() => {
+			debug("timer: execute");
+			resetSettings();
+			setupTimer();
+		}, settings.timeout * 1000);
+	}
 }
 
 function clearTimer() {
 	if (timer) {
+		debug("timer: clear");
 		clearTimeout(timer);
 	}
-}
-
-function resetSettings() {
-	const settings = getSettings();
-
-	// TODO: reset settings in appropriate file: local resetSettings config should clear local, user should clear user
-	// https://code.visualstudio.com/docs/getstarted/settings#_creating-user-and-workspace-settings
-
-	if (settings.notifyOfReset) {
-		vscode.window.showInformationMessage('Settings have been reset!');
-	}
-
-	resetTimer();
 }
 
 //#endregion
@@ -74,13 +94,25 @@ function resetSettings() {
 //#region HELPERS
 
 function getSettings() {
-	// TODO: get settings from json
+	// defined in package.json
+	const settings = vscode.workspace.getConfiguration('reset-settings-timeout');
+
 	return {
+		commands: [], // a list of string commands
 		settings: {}, // contains {key: value} from settings.json
-		timeout: 1*60*60,
+		timeout: 3600,
 		resetOnRestart: true,
 		notifyOfReset: false,
+		debug: false,
+		...settings,
 	};
+}
+
+function debug(...args: any[]) {
+	const settings = getSettings();
+	if (settings.debug) {
+		console.log("vscode-reset-settings-timeout: ", ...args);
+	}
 }
 
 //#endregion
